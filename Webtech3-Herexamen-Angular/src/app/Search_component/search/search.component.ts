06b9a6f07e8ca39b9b39291efc56804cecc82e55
Claus Frederic (s095602) <frederic.claus@student.ap.be>
2019-08-26 14:47:50 +0200

    Search functies angular

--- Webtech3-Herexamen-Angular/src/app/Search_component/search/search.component.ts
+++ Webtech3-Herexamen-Angular/src/app/Search_component/search/search.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import overtredingen from '../../../../overtredingen.json'
+import { OvertredingenService } from 'src/app/services/overtredingen.service.js';
 
 @Component({
   selector: 'app-search',
@@ -8,10 +9,19 @@ import overtredingen from '../../../../overtredingen.json'
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  overtredingenArray = [];
+
+  constructor(private overtredingService: OvertredingenService) { }
 
   ngOnInit() {
     console.log(overtredingen);
   }
 
+  onSubmitStraat(opnameplaats: string){
+    this.overtredingenArray = this.overtredingService.getSearchStraat(opnameplaats);
+  }
+
+  onSubmitSnelheid(aantalOvertredingen: Number){
+    this.overtredingenArray = this.overtredingService.getSearchAantalOvertredingen(aantalOvertredingen);
+  }
 }
--- Webtech3-Herexamen-Angular/src/app/services/overtredingen.service.ts
+++ Webtech3-Herexamen-Angular/src/app/services/overtredingen.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import overtredingen from '../../../overtredingen.json'
 import {Overtreding} from '../model/overtreding';
+import { ParsedEvent } from '@angular/compiler';
 
 @Injectable({
   providedIn: 'root'
@@ -16,4 +17,27 @@ export class OvertredingenService {
   })
   return this.overtredingenArray;
   }
+
+  getSearchStraat(text: string): Array<any[]>{
+    this.overtredingenArray = [];
+
+    overtredingen.forEach(Overtreding => {
+      if(text == Overtreding.opnameplaats_straat){
+        this.overtredingenArray.push(Overtreding);
+      }
+  })
+  return this.overtredingenArray;
+}
+
+getSearchAantalOvertredingen(nummer: Number): Array<any[]>{
+  this.overtredingenArray = [];
+
+  overtredingen.forEach(Overtreding => {
+    if(nummer <= Overtreding.aantal_overtredingen_snelheid){
+      this.overtredingenArray.push(Overtreding);
+    }
+})
+
+  return this.overtredingenArray;
+}
 }
